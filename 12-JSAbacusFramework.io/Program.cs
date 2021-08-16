4ac275af15d09598fb5b5d5c4d98895aba2a3d26
t-j <t-j@terence-john.co.uk>
2021-08-16 17:20:39 +0100

    * 12-JSAbacusFramework.io.csproj: 12 i&ii migrated from old repo
    * AoC15.sln:
    * input.txt:
    * Program.cs:
    * input.json:
    * input.txt:
    * Program.cs:
    * 13-KnightsOfTheDinnerTable.csproj:

---
+++ 12-JSAbacusFramework.io/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.IO;
+
+namespace _12_JSAbacusFramework.io
+{
+    class Program
+    {
+        private static string input;
+        private static int pos;
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("12-JSAbacusFramework.io\n-------------------------------- Part 1");
+            //{
+            //input = File.ReadAllText("input.json");
+
+            //int i = 0;
+            //int sum = 0;
+            //while (i < input.Length)
+            //{
+            //    if ((input[i] >= '0' && input[i] <= '9') || input[i] == '-')
+            //    {
+            //        // Parse out the number
+            //        string n = "" + (char)input[i++];
+            //        while (i < input.Length && input[i] >= '0' && input[i] <= '9')
+            //        {
+            //            n += (char)input[i++];
+            //        }
+            //        int num = int.Parse(n);
+            //        sum += num;
+            //        //                        Console.WriteLine($"{num,6}");
+            //    }
+            //    else
+            //        i++;
+            //}
+
+            //Console.WriteLine(sum);
+            //}
+
+            Console.WriteLine("\n-------------------------------- Part 2");
+            {
+                input = File.ReadAllText("input.txt");
+                //input = "[1,{\"c\":\"red\",\"b\":2},3]";
+                //input = "\"Hello World\",";
+
+                Console.WriteLine("         1         2         3");
+                Console.WriteLine(input);
+
+                EvalInput();
+
+            }
+        }
+        private static int EvalInput()
+        {
+            int sum = 0;
+
+            switch (input[SkipWhiteSpace()])
+            {
+                case ',':
+                case ':':
+                    pos++;
+                    break;
+                case '[':
+                    sum = EvalArray();
+                    break;
+                case '{':
+                    sum = EvalObject();
+                    break;
+                case '"':
+                    string str = EvalString();
+                    break;
+                default:
+                    sum = EvalNumber();
+                    break;
+            }
+            Console.WriteLine(sum);
+            return sum;
+        }
+        private static int EvalArray()
+        {
+            int sum = 0;
+
+            pos++;
+            while (input[SkipWhiteSpace()] != ']')
+            {
+                sum += EvalInput();
+                Console.WriteLine($"{pos,3}");
+            }
+            pos++;
+
+            return sum;
+        }
+        private static int EvalObject()
+        {
+            int sum = 0;
+
+            pos++;
+            while (input[SkipWhiteSpace()] != '}')
+            {
+                sum = EvalInput();
+                Console.WriteLine($"{pos,3}");
+            }
+            pos++;
+
+            return sum;
+        }
+        private static string EvalString()
+        {
+            string str = "";
+            pos++;
+            while (input[pos] != '"')
+                str += input[pos++];
+            pos++;
+
+            Console.WriteLine($"{pos,3} [{str}]");
+
+            return str;
+        }
+        private static int EvalNumber()
+        {
+            string str = "";
+            while (input[pos] == '-' || (input[pos] >= '0' && input[pos] <= '9'))
+                str += input[pos++];
+
+            Console.WriteLine($"{pos,3} [{str}]");
+
+            return int.Parse(str);
+        }
+        private static int SkipWhiteSpace()
+        {
+            while (input[pos] == ' ' || input[pos] == '\t' || input[pos] == '\n')
+                pos++;
+
+            return pos;
+        }
+    }
+}
---
+++ 13-KnightsOfTheDinnerTable/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace advent13
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string avail = "ABCDEFGM";
+            string[] strs = File.ReadAllLines("input.txt");
+
+            //string avail = "ABCD";
+            //string[] strs = {
+            //    "Alice would gain 54 happiness units by sitting next to Bob.",
+            //    "Alice would lose 79 happiness units by sitting next to Carol.",
+            //    "Alice would lose 2 happiness units by sitting next to David.",
+            //    "Bob would gain 83 happiness units by sitting next to Alice.",
+            //    "Bob would lose 7 happiness units by sitting next to Carol.",
+            //    "Bob would lose 63 happiness units by sitting next to David.",
+            //    "Carol would lose 62 happiness units by sitting next to Alice.",
+            //    "Carol would gain 60 happiness units by sitting next to Bob.",
+            //    "Carol would gain 55 happiness units by sitting next to David.",
+            //    "David would gain 46 happiness units by sitting next to Alice.",
+            //    "David would lose 7 happiness units by sitting next to Bob.",
+            //    "David would gain 41 happiness units by sitting next to Carol."
+            //        };
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach (string str in strs)
+            {
+                string[] flds = str.Split(' ');
+
+                int v = int.Parse((flds[2] == "lose" ? "-" : "+") + flds[3]);
+
+                //  string k = flds[0].Substring(0, 1) + flds[10].Substring(0, 1);
+                string k = flds[0][0].ToString() + flds[10][0].ToString();
+                if (dict.ContainsKey(k))
+                    dict[k] += v;
+                else
+                    dict.Add(k, v);
+
+                k = flds[10][0].ToString() + flds[0][0].ToString();
+                if (dict.ContainsKey(k))
+                    dict[k] += v;
+                else
+                    dict.Add(k, v);
+            }
+
+            Console.WriteLine("part 1-----------------------\n");
+            {
+                int mx = 0;
+                string mxstr = "";
+
+                foreach (List<char> perm in Permutate(avail.ToCharArray().ToList(), avail.Length))
+                {
+                    string comb = new string(perm.ToArray());
+                    comb += comb[0];
+
+                    int sum = 0;
+                    for (int i = 0; i < avail.Length; i++)
+                    {
+                        string op = comb.Substring(i, 2);
+                        //Console.Write(op + " " + dict[op] + " ");
+                        sum += dict[comb.Substring(i, 2)];
+                    }
+                    //Console.WriteLine($"    = {sum}");
+                    if (sum > mx)
+                    {
+                        mx = sum;
+                        mxstr = comb;
+                    }
+                }
+                Console.WriteLine($"{mxstr} {mx}");
+            }
+            Console.WriteLine("part 2-----------------------\n");
+            {
+                foreach (char ch in avail)
+                {
+                    dict.Add(ch + "T", 0);
+                    dict.Add("T" + ch, 0);
+                }
+                avail += 'T';
+
+                int mx = 0;
+                string mxstr = "";
+                foreach (List<char> perm in Permutate(avail.ToCharArray().ToList(), avail.Length))
+                {
+                    string comb = new string(perm.ToArray());
+                    comb += comb[0];
+
+                    int sum = 0;
+                    for (int i = 0; i < avail.Length; i++)
+                    {
+                        string op = comb.Substring(i, 2);
+                        Console.Write(op + " " + dict[op] + " ");
+                        sum += dict[comb.Substring(i, 2)];
+                    }
+                    Console.WriteLine($"    = {sum}");
+                    if (sum > mx)
+                    {
+                        mx = sum;
+                        mxstr = comb;
+                    }
+                }
+                Console.WriteLine($"{mxstr} {mx}");
+            }
+        }
+        /// <summary>
+        ///  --------------------------------------------------------------
+        /// </summary>
+        /// <param name="sequence"></param>
+        /// <param name="count"></param>
+        public static void RotateRight(IList sequence, int count)
+        {
+            object tmp = sequence[count - 1];
+            sequence.RemoveAt(count - 1);
+            sequence.Insert(0, tmp);
+        }
+
+        public static IEnumerable<IList> Permutate(IList sequence, int count)
+        {
+            if (count == 1) yield return sequence;
+            else
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    foreach (var perm in Permutate(sequence, count - 1))
+                        yield return perm;
+                    RotateRight(sequence, count);
+                }
+            }
+        }
+    }
+}
