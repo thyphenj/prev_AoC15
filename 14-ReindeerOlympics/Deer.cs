ef25796163f0e413899ae5cfa266d2e613283baf
t-j <t-j@terence-john.co.uk>
2021-08-22 15:34:48 +0100

    14 ii completed

---
+++ 14-ReindeerOlympics/Deer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace _14_ReindeerOlympics
+{
+    class Deer
+    {
+        private string _name;
+        private int _speed;
+        private int _flight;
+        private int _rest;
+        private int _distance;
+        private int _point;
+        public int Dist { get { return _distance; } }
+        public int Points { get { return _point; } }
+
+        public Deer(string name, int speed, int flight, int rest, int time)
+        {
+            _name = name;
+            _speed = speed;
+            _flight = flight;
+            _rest = rest;
+
+            _distance = _part1_distance(time);
+
+            _point = 0;
+        }
+        private int _part1_distance(int time)
+        {
+            int phaseTime = _flight + _rest;
+
+            int phases = 0;
+            while (time > phaseTime)
+            {
+                time -= phaseTime;
+                phases++;
+            }
+
+            if (time > _flight)
+                time = _flight;
+
+            int retval = (time + (phases * _flight)) * _speed;
+
+            return retval;
+        }
+
+        public int Position(int secs)
+        {
+            int phases = secs / (_flight + _rest);
+            int rem = secs % (_flight + _rest);
+            if (rem <= _flight)
+                return (phases * _flight + rem) * _speed;
+            else
+                return (phases * _flight + _flight) * _speed;
+        }
+
+        public void AddPoint()
+        {
+            _point++;
+        }
+        public override string ToString()
+        {
+            return $"{_name,10}   -   {_distance}";
+        }
+    }
+
+}
---
+++ 14-ReindeerOlympics/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace _14_ReindeerOlympics
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            int time = 2503;
+
+            List<Deer> deers = ReadInput(time);
+
+            Deer maxDeer = deers[0];
+            foreach (Deer deer in deers)
+            {
+                if (deer.Dist > maxDeer.Dist)
+                    maxDeer = deer;
+
+                Console.WriteLine(deer);
+            }
+            Console.WriteLine("\n------- furthest -------\n");
+            Console.WriteLine(maxDeer);
+
+            for (int secs = 1; secs <= time; secs++)
+            {
+                Deer leader = null;
+                double maxPosition = 0;
+                foreach (Deer deer in deers)
+                {
+                    int position = deer.Position(secs);
+                    if (position > maxPosition)
+                    {
+                        maxPosition = position;
+                        leader = deer;
+                    }
+                }
+                leader.AddPoint();
+                Console.WriteLine($"{secs,5}   leader - {leader,10} - points - {leader.Points}");
+            }
+
+            Console.WriteLine();
+            foreach (Deer deer in deers)
+            {
+                Console.WriteLine($"{deer,10} points={deer.Points,4}");
+            }
+
+        }
+
+        public static List<Deer> ReadInput(int time)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+
+            List<Deer> deers = new List<Deer>();
+            foreach (string line in lines)
+            {
+                string[] atoms = line.Split(' ');
+
+                int speed = int.Parse(atoms[3]);
+                int flight = int.Parse(atoms[6]);
+                int rest = int.Parse(atoms[13]);
+                deers.Add(new Deer(atoms[0], speed, flight, rest, time));
+            }
+            return deers;
+        }
+    }
+}
