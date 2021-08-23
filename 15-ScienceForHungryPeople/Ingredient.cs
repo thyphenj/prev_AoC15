48ad524b0d896aebc4659187f7f99dbeae6c46e8
t-j <t-j@terence-john.co.uk>
2021-08-23 14:31:40 +0100

    15 - init commit

---
+++ 15-ScienceForHungryPeople/Ingredient.cs
@@ -0,0 +1,31 @@
+﻿//Chocolate: capacity 0, durability 0, flavor -2, texture 2, calories 8
+using System;
+namespace _15_ScienceForHungryPeople
+{
+    public class Ingredient
+    {
+        public string Name;
+        public int Capacity;
+        public int Durability;
+        public int Flavour;
+        public int Texture;
+        public int Calories;
+
+        public Ingredient(string line)
+        {
+            Name = line.Substring(0, line.IndexOf(':'));
+            string[] atoms = line.Split(':')[1].Split(',');
+
+            Capacity = int.Parse(atoms[0].Trim().Split(' ')[1]);
+            Durability = int.Parse(atoms[1].Trim().Split(' ')[1]);
+            Flavour = int.Parse(atoms[2].Trim().Split(' ')[1]);
+            Texture = int.Parse(atoms[3].Trim().Split(' ')[1]);
+            Calories = int.Parse(atoms[4].Trim().Split(' ')[1]);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name,10} : Capacity {Capacity,2}, Durability {Durability,2}, Flavour {Flavour,2}, Texture {Texture,2}, Calories {Calories,2}";
+        }
+    }
+}
---
+++ 15-ScienceForHungryPeople/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace _15_ScienceForHungryPeople
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Ingredient> ingredients = ReadIngredients("input.txt");
+
+            int i = 1;
+            foreach (var line in ingredients)
+                Console.WriteLine($"{i++,2}) {line}");
+
+
+        }
+
+        private static List<Ingredient> ReadIngredients(string filename)
+        {
+            string[] str = File.ReadAllLines(filename);
+            List<Ingredient> ingredients = new List<Ingredient>();
+
+            foreach (string s in str)
+            {
+                ingredients.Add(new Ingredient(s));
+            }
+            return ingredients;
+        }
+    }
+}
